28df813a07c61765f4e7d18acfbf87da22c33140
Luiz Felipe Gonçalves <git@luizfelipe.dev>
2025-06-07 21:52:18 -0300

    Organize lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,15 @@
-pub mod ast;
+/// The lexer takes the source input, mapping it into a sequence of tokens.
 pub mod lexer;
+
+/// The parser takes a sequence of tokens, mapping it into an AST.
 pub mod parser;
-pub mod token;
+
+/// The type checker takes an untyped AST, checks the soundness of its types,
+/// and maps it into a typed AST.
 pub mod type_checker;
+
+pub mod ast;
+pub mod token;
 pub mod types;
 
 pub mod util {
